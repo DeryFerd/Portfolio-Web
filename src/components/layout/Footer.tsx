396529641bd14f5bfd40c195d71a7f850f221fc6
DeryFerd <deryferdikao125@gmail.com>
2026-04-01 01:08:02 +0700

    Limit footer rendering to the homepage

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "./Footer.module.css";
 
 const socialLinks = [
@@ -8,6 +11,12 @@ const socialLinks = [
 ];
 
 export default function Footer() {
+  const pathname = usePathname();
+
+  if (pathname !== "/") {
+    return null;
+  }
+
   return (
     <footer className={styles.footer} data-robot-avoid>
       <div className="container">
